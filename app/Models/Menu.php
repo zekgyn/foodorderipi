0f39eddf5a6a4276deaee274e768082bd28cc17b
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-06-25 08:09:22 +0300

    relationship between order and menu

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Order;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Menu extends Model
 {
@@ -13,4 +14,8 @@ class Menu extends Model
         'price',
         'image'
     ];
+    protected function orders()
+    {
+        $this->belongsToMany(Order::class);
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Menu;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
     use HasFactory;
+    protected $fillable = ['menu_id', 'phone','location'];
+
+    protected function menus()
+    {
+        $this->hasOne(Menu::class);
+    }
 }
