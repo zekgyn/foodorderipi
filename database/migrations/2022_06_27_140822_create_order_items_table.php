f38af80956e59a5e2c8897530019b97d40163a2c
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-06-30 20:20:11 +0300

    Create 2022_06_27_140822_create_order_items_table.php

---
+++ database/migrations/2022_06_27_140822_create_order_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('price');
+            $table->string('image')->nullable();
+            $table->string('name');
+            $table->foreignid('menu_id')->nullable()->constrained('menus')->cascadeOnDelete();
+            $table->foreignid('order_id')->constrained('orders')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
