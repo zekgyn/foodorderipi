058ad2f5abbcfab1454860745109fa17dc87e7ee
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-06-30 20:18:58 +0300

    random order number + item relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,16 +3,29 @@
 namespace App\Models;
 
 use App\Models\Menu;
+use App\Models\OrderItem;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
     use HasFactory;
-    protected $fillable = ['menu_id', 'phone','location'];
 
-    protected function menus()
+    protected $fillable = [
+        'order_number',
+        'is_placed'
+    ];
+    // sgenerate order numbber
+    protected static function boot()
     {
-        $this->hasOne(Menu::class);
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->order_number = strtoupper(bin2hex(openssl_random_pseudo_bytes(5, $cstrong)));
+        });
+    }
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
     }
 }
