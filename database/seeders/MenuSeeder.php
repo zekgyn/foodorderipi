57c6c8891a4f97f8cfb89ebc2c9a584fcaaa2175
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-07-01 12:17:37 +0300

    menu seeder realistic random titles

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -48,12 +48,33 @@ class MenuSeeder extends Seeder
     {
         // $json = Storage::disk('local')->get('/json/menu.json');
         $json = file_get_contents(base_path("/database/seeders/json/menu.json"));
-        $menus = json_decode($json, true);
-        foreach ($menus as $menu) {
-            $menu = Menu::create([
-                'title' => $this->faker->words(8, true),
-                'price' => $this->faker->randomElement([1000, 1500, 2000, 2500]),
-                'image' => $menu['image']
+        $images = json_decode($json, true);
+        foreach ($images as $image) {
+            Menu::create([
+                'title' => $this->faker->unique()->randomElement([
+                    'Mixer Nyama full',
+                    'Mixer Samaki full',
+                    'Wali Samaki full',
+                    'Ugali Samaki full',
+                    'Wali nyama full',
+                    'wali mbogamboga full',
+                    'ugali nyama full',
+                    'ugali mbogamboga full',
+                    'Ndizi Samaki full',
+                    'Ndizi Samaki full',
+                    'Mixer kuku full',
+                    'ugali kuku full',
+                    'wali kuku full',
+                    'ndizi kuku full',
+                    'chapati Samaki full',
+                    'chapati kuku full',
+                    'chapati nyama full',
+                    'Biryan nyama full',
+                    'Biryan kuku full'
+
+                ]),
+                'price' => $this->faker->randomElement([1000, 1500, 2000, 2500, 3000, 3500, 4000.4500]),
+                'image' => $image['image']
             ]);
         }
     }
