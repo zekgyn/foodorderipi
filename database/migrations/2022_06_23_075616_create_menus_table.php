b44808faab85971c6071119391a2af48b1f2c8c9
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-07-01 12:15:19 +0300

    make menu title unique

--- database/migrations/2022_06_23_075616_create_menus_table.php
+++ database/migrations/2022_06_23_075616_create_menus_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title')->unique();
             $table->unsignedDecimal('price');
             $table->string('image');
             $table->timestamps();
