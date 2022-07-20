60650be86a0fef6bc0698f5f7236dc5da3153cb6
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-07-20 10:40:37 +0300

    menu uuid

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -3,17 +3,44 @@
 namespace App\Models;
 
 use App\Models\Order;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Menu extends Model
 {
     use HasFactory;
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
     protected $fillable = [
         'title',
         'price',
         'image'
     ];
+
+    // static function for generating uuid
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
+
+    public function resolveRouteBinding($value, $field = null)
+    {
+
+        if (!Str::isUuid($value)) {
+            throw (new ModelNotFoundException)->setModel(Menu::class, $value);
+        }
+
+        return $this->where("id", $value)->firstOrFail();
+    }
     // public function orders()
     // {
     //     $this->belongsToMany(Order::class);
--- database/migrations/2022_06_23_075616_create_menus_table.php
+++ database/migrations/2022_06_23_075616_create_menus_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,7 +13,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('menus', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('title')->unique();
             $table->unsignedDecimal('price');
             $table->string('image');
