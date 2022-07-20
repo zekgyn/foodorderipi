62f9fc9c01cbc35476c5e841ab128fb776f0acde
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-07-20 10:44:46 +0300

    user uuid

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    // static function for generating uuid and order number
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid(); //generate uuid
+            $model->order_number = strtoupper(bin2hex(openssl_random_pseudo_bytes(5, $cstrong))); //generate order number
+        });
+    }
     /**
      * The attributes that are mass assignable.
      *
@@ -22,7 +38,13 @@ class User extends Authenticatable
         'email',
         'password',
     ];
-
+    public function resolveRouteBinding($value, $field = null)
+    {
+        if (!Str::isUuid($value)) {
+            throw (new ModelNotFoundException)->setModel(Order::class, $value);
+        }
+        return $this->where("id", $value)->firstOrFail();
+    }
     /**
      * The attributes that should be hidden for serialization.
      *
