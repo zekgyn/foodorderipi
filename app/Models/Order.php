e48240b5ff6165b6d889679024e32380d3ae63d3
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-07-20 10:41:00 +0300

    order uuid

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,26 +4,42 @@ namespace App\Models;
 
 use App\Models\Menu;
 use App\Models\OrderItem;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Order extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'order_number',
-        'is_placed'
-    ];
-    // sgenerate order numbber
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    // static function for generating uuid and order number
     protected static function boot()
     {
         parent::boot();
 
         static::creating(function ($model) {
-            $model->order_number = strtoupper(bin2hex(openssl_random_pseudo_bytes(5, $cstrong)));
+            $model->id = (string) Str::uuid(); //generate uuid
+            $model->order_number = strtoupper(bin2hex(openssl_random_pseudo_bytes(5, $cstrong))); //generate order number
         });
     }
+    protected $fillable = [
+        'order_number',
+        'is_placed'
+    ];
+
+    public function resolveRouteBinding($value, $field = null)
+    {
+        if (!Str::isUuid($value)) {
+            throw (new ModelNotFoundException)->setModel(Order::class, $value);
+        }
+        return $this->where("id", $value)->firstOrFail();
+    }
+
     public function orderItems()
     {
         return $this->hasMany(OrderItem::class);
--- database/migrations/2022_06_23_120532_create_orders_table.php
+++ database/migrations/2022_06_23_120532_create_orders_table.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -16,7 +15,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('order_number')->unique();
             $table->boolean('is_placed')->default(false);
             $table->timestamps();
