d47e513759677ff6dbc1d4b7230136ba02bd41de
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-07-31 08:23:52 +0300

    new routes + naming best practice

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController;
-use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\OrderController;
-
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,35 +19,37 @@ use App\Http\Controllers\OrderController;
 */
 Route::post('login', [LoginController::class, 'login']);
 
-Route::group(['middleware' => 'auth:sanctum'],function () {
+Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('logout', [LoginController::class, 'logout']);
 
     //menus
     Route::get('menu', [MenuController::class, 'index']);
-    Route::get('menunopg', [MenuController::class, 'indexnopg']);
+    Route::get('menu_all', [MenuController::class, 'indexall']);
+    Route::post('create_menu', [MenuController::class, 'store']);
+    Route::put('update_menu/{menu}', [MenuController::class, 'update']);
+    Route::delete('delete_menu/{menu}', [MenuController::class, 'destroy']);
 
-    Route::post('menu', [MenuController::class, 'store']);
+    //employees
+    Route::get('employees', [EmployeeController::class, 'index']);
+    Route::get('employees_all', [EmployeeController::class, 'indexall']);
+    Route::post('create_employee', [EmployeeController::class, 'store']);
+    Route::put('update_employee/{employee}', [EmployeeController::class, 'update']);
+    Route::delete('delete_employee/{employee}', [EmployeeController::class, 'destroy']);
 
-    //create orders
-    Route::post('order', [OrderController::class, 'store']);
-
-    // update order
-    Route::put('order_update/{order}', [OrderController::class, 'update']);
-
-    //open orders
-    Route::get('open_orders', [OrderController::class, 'index']);
-    Route::get('open_order/{order}', [OrderController::class, 'show']);
+    //orders
+    Route::get('orders', [OrderController::class, 'index']);
+    Route::get('show_order/{order}', [OrderController::class, 'show']);
+    Route::post('create_order', [OrderController::class, 'store']);
+    Route::put('update_order/{order}', [OrderController::class, 'update']);
 
     // send order
     Route::post('send_order', [OrderController::class, 'send']);
 
+    //reports
+    Route::get('report', [OrderController::class, 'report']);
 
-    //closed orders
-    Route::get('closed_orders', [OrderController::class, 'indexClosed']);
-    Route::get('closed_order/{order}', [OrderController::class, 'closedShow']);
-
-
-});;
+});
+;
 
 // Route::apiResource();
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
