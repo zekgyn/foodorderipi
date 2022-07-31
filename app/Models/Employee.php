8b22a3b03d04a808bb1340f99325987645d3f611
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-07-31 07:14:13 +0300

    Update Employee.php
    employee model logic

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'name',
+        'phone',
+    ];
+
+    // static function for generating uuid
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
+
+    public function resolveRouteBinding($value, $field = null)
+    {
+        if (!Str::isUuid($value)) {
+            throw (new ModelNotFoundException)->setModel(Employee::class, $value);
+        }
+        return $this->where("id", $value)->firstOrFail();
+    }
 }
