62d1c9bdc44fbe8a57b556407ce5f58b3ab096f9
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-07-31 07:14:38 +0300

    Update 2022_07_30_153200_create_employees_table.php
    employee first migration

--- database/migrations/2022_07_30_153200_create_employees_table.php
+++ database/migrations/2022_07_30_153200_create_employees_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('employees', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('phone')->unique();
             $table->timestamps();
         });
     }
