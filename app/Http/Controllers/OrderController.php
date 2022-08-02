9f4ebfd1af3a3514f355afa065112e73ca7cf2d3
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-08-02 20:40:33 +0300

    Update OrderController.php
    report logic

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Menu;
 use App\Jobs\SendSms;
+use App\Models\Order;
+use App\Models\Report;
+use App\Models\OrderItem;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Auth\Events\Validated;
 use App\Http\Requests\SendOrderRequest;
 use Illuminate\Validation\Rules\Exists;
 use App\Http\Requests\StoreOrderRequest;
 use App\Http\Requests\UpdateOrderRequest;
-use App\Models\OrderItem;
-use Illuminate\Auth\Events\Validated;
-use App\Models\Order;
 
 class OrderController extends Controller
 {
@@ -26,15 +28,28 @@ class OrderController extends Controller
 
         return $orders;
     }
-    // public function indexClosed()
-    // {
-    //     $orders = Order::where([
-    //         'is_placed' => true
-    //     ])->orderby('created_at', 'desc')->paginate(10);
 
-    //     return $orders;
-    // }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function show($order)
+    {
+        $result = Order::select('id', 'order_number', 'created_at')->where('id', $order)
+        ->with('orderItems:id,employee_id,menu_id,order_id')
+        ->orderBy('created_at')->get();
+        // $order->with('orderItems:id,employee_id,menu_id,order_id')->get();
+        // $result = $order->with('orderItems')->get();
 
+    //    $order->with(['orderItems:id' => function ($q) use ($order) {
+    //         $q->wherePivot('menu_id', '=', $order);
+    //     }])->first();
+
+        return $result;
+        // return response()->json($result);
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -52,16 +67,15 @@ class OrderController extends Controller
             ]);
             //Loop menu items and insert into the database
             foreach ($validated['menus'] as $menuitem) {
-                $menu = Menu::select('id', 'price', 'image', 'title')
+                $menu = Menu::select('id', 'price', 'title')
                     ->where('id', $menuitem['menu_id'])
                     ->first();
                 // Create order items for new order
                 $order->orderItems()->create([
                     'menu_id' => $menu['id'],
-                    'title' => $menu['title'],
-                    'name' => $menuitem['name'],
-                    'image' => $menu['image'],
-                    'price' => $menu['price']
+                    'employee_id' => $menuitem['employee_id'],
+                    // 'image' => $menu['image'],
+                    // 'price' => $menu['price']
                 ]);
             }
         });
@@ -79,13 +93,13 @@ class OrderController extends Controller
     public function send(SendOrderRequest $request)
     {
         $validated = $request->validated();
-
         //change order status in db
         $send = DB::transaction(function () use ($validated) {
             $order = Order::find($validated['order_id']);
             $order->is_placed = true;
             $order->save();
-        });
+
+
         //retrieve created order
         if (Order::where([
             'id' => $validated['order_id'],
@@ -99,7 +113,7 @@ class OrderController extends Controller
             // send sms to restaurant and buyer
             $textrestaurant = "Order# {$order['order_number']}";
             // return response()->json(["order" => $textrestaurant]);
-            $items = $order->load('orderItems:id,order_id,title,name');
+            $items = $order->load('orderItems:id,order_id,menu_id');
             foreach ($items['orderItems'] as $item) {
 
                 // return response()->json(["item" => $item]);
@@ -109,36 +123,12 @@ class OrderController extends Controller
             // SendSms::dispatch($textrestaurant, 255620170041);
             // return response()->json(["order" => $textrestaurant]);
         }
-
+        });
         // return response to client
         return response()->json([
             'response' => ['order' => 'Order sent successfully']
         ]);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Order $order)
-    {
-        $result = $order->load('orderItems:id,order_id,title,name');
-
-        return response()->json($result);
-    }
-
-    // public function closedShow(Order $order)
-    // {
-
-    //     if ($order->is_placed == true) {
-    //         $result = $order->load('orderItems:id,order_id,title,name');
-    //         return response()->json($result);
-    //     } else {
-    //         return response()->json(["message" => "Order is still open"]);
-    //     }
-    // }
 
     /**
      * Update the specified resource in storage.
@@ -151,40 +141,57 @@ class OrderController extends Controller
     {
         $validated = $request->validated();
         if (!$order->is_placed == true) {
-        $order = DB::transaction(function () use ($validated, $order) {
+            $order = DB::transaction(function () use ($validated, $order) {
 
-    // Add order items
-    if (!empty($validated['add_menus'])) {
-        foreach ($validated['add_menus'] as $data) {
-            $menu = Menu::select('id', 'price', 'image', 'title')
-            ->where('id', $data['menu_id'])
-            ->first();
-            $order->orderItems()->create([
-            'menu_id' => $menu['id'],
-            'title' => $menu['title'],
-            'name' => $data['name'],
-            'image' => $menu['image'],
-            'price' => $menu['price']
+                // Add order items
+                if (!empty($validated['add_item'])) {
+                    foreach ($validated['add_item'] as $data) {
+                        $menu = Menu::where('id', $data['menu_id'])
+                            ->first();
+
+                        $order->orderItems()->create([
+                                'menu_id' => $menu['id'],
+                                'employee_id' => $data['employee_id'],
+                                // 'title' => $menu['title'],
+                                // 'image' => $menu['image'],
+                                // 'price' => $menu['price']
                     ]);
-        }
-    }
-    // Delete order items
-        if (!empty($validated['delete_menus'])) {
-            foreach ($validated['delete_menus'] as $id) {
-                OrderItem::where('id', $id)->delete();
-         }
-    }
-
-
-        });
+                    }
+                }
+                // Delete order items
+                if (!empty($validated['delete_item'])) {
+                    foreach ($validated['delete_item'] as $id) {
+                        OrderItem::where('id', $id)->delete();
+                    }
+                }
+            });
             // return response to client
             return response()->json([
                 'response' => ['order' => 'Order updated successfully']
             ]);
-  }else {
+        } else {
             return response()->json(['error'=>'Updating a complete order is not allowed'], 422);
         }
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function report()
+    {
+        if (OrderItem::filterByDate(request('start_date'),request('end_date'))
+                ->orderBy('created_at')->exists()) {
+            $report = Order::select('id','order_number','created_at')->filterByDate(request('start_date'), request('end_date'))
+                ->with('orderItems:id,employee_id,menu_id,order_id')
+                ->orderBy('created_at')->get();
 
+            return response()->json($report);
+
+        }
+        else {
+            return response()->json([],404);
+        }
 
     }
 }
