2c9379ae63fdbd2870b535768c372df0e5b0aeec
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-08-22 18:06:44 +0300

    Create 2022_08_20_165558_create_employee_menu_items_table.php

---
+++ database/migrations/2022_08_20_165558_create_employee_menu_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_menu_items', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('order_item_id')->constrained('order_items','id')->nullOnDelete();
+            $table->foreignUuid('menu_id')->constrained('menus')->nullOnDelete();
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_menu_items');
+    }
+};
