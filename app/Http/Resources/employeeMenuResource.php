ac68dd468d1380e90976dfaacefabc8430dd4418
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-08-22 18:23:23 +0300

    Create employeeMenuResource.php

---
+++ app/Http/Resources/employeeMenuResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Menu;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class employeeMenuResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $menu = Menu::select('price','title')->where('id', $this->menu_id)->first();
+        return [
+            'id' =>$this->id,
+            'title' => $menu->title,
+            'price' => $menu->price,
+            'qty' => $this->quantity,
+
+        ];
+    }
+}
