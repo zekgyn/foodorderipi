5937f8804aad33277a12631c85975be9cd844f07
Juma Yunus <73176621+zekgyn@users.noreply.github.com>
2022-08-29 18:21:51 +0300

    Update 2022_08_20_165558_create_employee_menu_items_table.php

--- database/migrations/2022_08_20_165558_create_employee_menu_items_table.php
+++ database/migrations/2022_08_20_165558_create_employee_menu_items_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('employee_menu_items', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignUuid('order_item_id')->constrained('order_items','id')->nullOnDelete();
+            $table->foreignUuid('order_item_id')->constrained('order_items', 'id')->cascadeOnDelete();
             $table->foreignUuid('menu_id')->constrained('menus')->nullOnDelete();
             $table->integer('quantity');
             $table->timestamps();
